48c867c8a587319e7ad247b6d6f0b14af42baaf2
Abhisheksharma0886 <147491858+Abhisheksharma0886@users.noreply.github.com>
2026-07-01 09:38:28 +0530

    Fix database syntax error

--- ssvmt_project/settings.py
+++ ssvmt_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ssvmt_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600
     )
 }
