cb92e06aa80990ba593d0a2753843712fa2fa8db
Abhisheksharma0886 <147491858+Abhisheksharma0886@users.noreply.github.com>
2026-07-01 10:24:04 +0530

    Cleaned settings and fixed syntax error

--- ssvmt_project/settings.py
+++ ssvmt_project/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +83,6 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
